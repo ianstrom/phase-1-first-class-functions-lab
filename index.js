a3a604d47c2e8b20cdc363ce1d031ed44c98cea8
ianstrom <iankstrom@gmail.com>
2023-01-31 12:58:56 -0500

    Finishing course work

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function() {
+    let firstTwo = []
+    for (let i = 0; i < 2; i++) {
+        firstTwo.push(drivers[i])
+    }
+    return firstTwo;
+}
+
+const returnLastTwoDrivers = function() {
+    let lastTwo = []
+    for (let i = 2; i < drivers.length; i++) {
+        lastTwo.push(drivers[i])
+    }
+    return lastTwo;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare) {
+    return function fareMultiplier(num) {
+        return fare * num;
+    };
+};
+
+function fareDoubler(fare) {
+    return fare * 2;
+};
+
+function fareTripler(fare) {
+    return fare * 3;
+};
+
+function selectDifferentDrivers(drivers, fn) {
+    if (fn === returnFirstTwoDrivers) {
+       return returnFirstTwoDrivers();
+    } else {
+       return returnLastTwoDrivers();
+    }
+}
